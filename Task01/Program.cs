117cb7a82d1442cea543ce14bc644acb032067a2
abyanji <a.byanjil99@gmail.com>
2022-12-26 01:36:39 +0300

    Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+int DataEntry(string message)
+{
+    System.Console.Write(message);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
+
+int[,] CreateRandomArray(int rows, int columns, int leftRange, int rightRange)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(leftRange, rightRange);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write($"{array[i, j]} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int m = DataEntry("Введите значение M: ");
+int n = DataEntry("Введите значение N: ");
+int[,] matrix = CreateRandomArray(m, n, 0, 10);
+PrintArray(matrix);
